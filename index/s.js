86b008ae23e6170aff52512752baa7eaec0ccd12
Natalia Iancovscaia <Natalia.Iancovscaia@medieinstitutet.se>
2024-10-28 22:40:31 +0100

    first commit

---
+++ index/s.js
@@ -0,0 +1,168 @@
+/*console.log(`hello,world`);
+let email=`nirute@gmail.com`;
+console.log(email);
+let lastName=`Kale`;
+let firsName=`Brendon`;
+let fullName=lastName+ ` `+firsName;
+console.log(fullName);
+console.log(fullName[2]);
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
+//let result=fullName.toLowerCase();
+//console.log(result);
+
+let index=email.indexOf(`@`);
+console.log(index);
+//let result=email.lastIndexOf(`i`);
+//console.log(result);
+//let result=email.slice(5,7);
+//console.log(result);
+//let result=email.substr(5,1);
+//console.log(result);
+let result=email.replace(`n`,`w`);
+console.log(result);
+let radius=10;
+const pi=3.14;
+console.log(radius,pi);
+console.log(10/2);
+//let result=radius%3;
+//let result=pi*radius**2;
+//console.log(result);
+let likes=10;
+//likes=10+1;
+//likes++;
+//likes--;
+//likes+=10;
+//likes-=5;
+let result=`The blog has `+likes+`likes`;
+console.log(result);
+const title=`Best reads`;
+const author=`Mario`;
+const likes=10;
+//let result=`The blog called `+title+` by `+author+` has `+likes+` likes `;
+//let result= `The blog called ${title} by ${author} has ${likes}  likes `;
+//console.log(result);
+let html=`
+<h2> ${title}</h2>
+<p>${author}</p>
+`;
+console.log(html);*/
+let ninjia=[`shaun`,`ryu`,`chun-li`];
+//console.log(ninjia[1]);
+/*ninjia[1]=`ken`;
+console.log(ninjia[1]);
+let ages=[20,25,30,35];
+console.log(ages[2]);
+let random=[`shaun`,`ryu`,20,30];
+console.log(random);
+console.log(ninjia.length);
+let result=ninjia.indexOf(`chun-li`);
+console.log(result);
+let result=ninjia.concat(`ken`,`mai`);
+console.log(result);
+let result=ninjia.push(`ken`);
+result=ninjia.pop();
+console.log(result);
+let age=null;
+console.log(age,age+3,`the age is ${age}`);
+console.log(true,false);
+let email="luige@thenetninjia.co.uk";
+let result=email.includes(`@`);
+
+let names=[`mariu`,`luige`,`ted`];
+ result=names.includes(`mariu`);
+console.log(result);
+let age=25;
+console.log(age==25);
+/*console.log(age==30);
+console.log(age!=30);
+console.log(age>20);
+//let name=`shon`;
+//console.log(name==`shon`);
+console.log(age==`25`);
+let score=`100`;
+score=Number(score);
+console.log(score +1);
+console.log(typeof score);
+let names=`hello`;
+names=Number(names);
+console.log(names);
+let result =Boolean(100);
+console.log(result,typeof result);
+for( let i=0;i<5;i++ ) {
+    console.log(`in lopp`,i);
+
+}
+const names=[`shon`,`mario`,`liugi`];
+for(let i=0;i<names.length;i++){
+    //console.log(i);
+    console.log(names[i]);
+}
+let i=0;
+while(i<5){
+    console.log(`in loop`,i);
+    i++;
+}
+let i=5;
+while(i<5){
+    console.log(`val of i is `,i);
+    i++;
+}
+let i=3;
+do {console.log(`value of i is`,i)
+    i++;
+}
+while(i<5);
+const age=19;
+if (age>20){console.log(`you  are over 20 years`);
+
+}
+const ninja=[`shon`,`ruy`,`chin`,`li`];
+if (ninja.length>3){
+    console.log("that s a lot of ninja");
+}
+const password=`pashccccvbccnccc`;
+if (password.length>=12&&password.includes(`@`))
+{console.log(`This password much long`);
+
+} else  if(password.length>8||password.includes(`@`))
+{console.log(`this pass is  long`);} else
+{console.log(`this pass is not long`);}
+let user=false;
+if (!user){
+    console.log(`you must be loggad`);}
+const scores=[50,20,30,100,23,89];
+for(let i=0;i<scores.length;i++){
+    if (scores[i]===0){continue;}
+    console.log("your score is",scores[i]);
+    if (scores[i]===100){
+        console.log("you got top score");
+        break;
+    }
+}
+const grade=`C`;
+switch(grade){
+    case`A`:console.log(`you have A`);
+    break;
+    case`B`:console.log(`you have B`);
+    break;
+    case`C`:console.log(`you have C`);
+    break;
+    case`D`:console.log(`you have D`);
+    break;
+    default:console.log(`not valid`);
+    
+}*/
+let age=30;
+if (true){
+    let age=40;
+    let names=`shon`;
+    console.log(`In the block`,age,names);
+    if(true){
+        let age=80;
+         let names =`Mih`;
+        console.log(`in block block`,age ,names);
+    }
+
+}
+console.log(`Outside block`,age);
---
+++ index/work.js
@@ -0,0 +1,119 @@
+/**
+ * Workshop: Password Checker
+ *
+ * Skriv kod som kollar att lösenordet i variabeln password har
+ * - minst 6 tecken varav minst två specialtecken enligt nedan
+ * - minst 8 tecken varav minst ett specialtecken enligt nedan
+ * - eller har minst 12 tecken och minst 1 bindestreck
+ * - eller har minst 16 tecken
+ *
+ * Börja med att lösa den i omvänd ordning, dvs få först kollen att lösenordet
+ * har minst 16 tecken att fungera.
+ *
+ * Utöka därefter koden till att kolla om lösenordet har minst 12 tecken OCH
+ * minst 1 bindestreck.
+ *
+ * Osv.
+ *
+ * Googla för att hitta lämpliga metoder för att lösa uppgiften.
+ *
+ * Du ska alltså precis som i första dagens workshop ta bort kommentarerna
+ * en efter en och testa att alla olika kombinationer av lösenord fungerar
+ * och ger rätt resultat.
+ */
+
+let password;
+// password = "password"; // inte giltigt
+// password = "pa$sword"; // giltigt
+ password = "p@ssw%rd"; // giltigt
+ //password = "paxc!word"; // giltigt
+//password = "secretpassword"; // inte giltigt
+ //password = "secret-password"; // giltigt
+//password = "such-password-much-secure-very-long"; // giltigt
+
+const specialChars = [
+	"@", "$", "%", "*", "^", "<", ">", "?", "!", "(", ")", "[", "]", "{", "}", "'"
+];
+
+console.log(`🕵🏻 Checking password '${password}'`);
+/*1 
+
+if (password.length>16){
+    console.log(`  password har minst 16 tecken
+ *`);
+}else   if (password.length>12 && password.includes(`-`)){
+    console.log("Your password have enough lenght and has `-`");
+}
+else{
+    console.log("You need to add letters or `-`");
+}*/
+
+
+/*3
+for (let i = 0; i < specialChars.length; i++) {
+    if (password.includes(specialChars[i])) {
+        console.log(`The specialCharacters "${specialChars[i]}"is in the password "${password}"`);
+        break; 
+    }else
+    console.log(`The specialCharacters "${specialChars[i]}" is not in the password "${password}"`);
+    continue;
+}*/
+
+
+
+/*let contains = false;
+
+
+for (let i = 0; i < specialChars.length; i++) {
+	if (password.includes(specialChars[i])) {
+		contains = true;
+		break; 
+	}
+}
+
+if (contains) {
+	console.log("Password contains special characters.");
+} else {
+	console.log("Password does not contain special characters.");
+}
+
+let contains = false;
+
+
+for (let i = 0; i < specialChars.length; i++) {
+	if (password.includes(specialChars[i])) {
+		contains = true;
+		break;
+	}
+}
+
+if (contains) {
+	console.log("Password contains special characters.");
+} else {
+	console.log("Password does not contain special characters.");
+}*/
+for (let i = 0; i < specialChars.length; i++) {
+    const con = password.includes(specialChars[i]);
+    let specialCharCount = 0;;
+  
+    if (password.length >= 16) {
+      console.log("Password: Giltigt, långt också.");
+    } else if (password.length >= 12 && password.includes("-")) {
+      console.log("Password: Giltigt, password.length >= 12 && password.includes - ");
+    } //else if (con && password.length >= 8) {
+      //console.log("Giltigt: Minst 8 bokstäver och specialtecken");
+    //}
+    else  
+
+    for (let i = 0; i < specialChars.length; i++) {
+        if (password.includes(specialChars[i])) {
+            specialCharCount++;
+        }
+    }
+    
+    if (specialCharCount >= 2) {
+        console.log(" Password contains at least two special characters.");
+    } 
+    else {console.log("password är inte giltigt")}
+    }
+
